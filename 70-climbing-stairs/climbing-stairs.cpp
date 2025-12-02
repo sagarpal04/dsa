726c3b5b03839895fab78e391808d985e9da459a
sagarpal04 <125257733+sagarpal04@users.noreply.github.com>
2025-12-02 21:48:28 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (33.71%) - LeetSync

---
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    vector<int> dp;
+    int solve(int i, int n){
+        if(i == n) return 1;
+        if(i > n) return 0;
+        if(dp[i] != -1) return dp[i];
+        return dp[i] = solve(i + 1, n) + solve(i + 2, n);
+    }
+    int climbStairs(int n) {
+        dp.assign(n + 1, -1);
+        return solve(0, n);
+    }
+};
