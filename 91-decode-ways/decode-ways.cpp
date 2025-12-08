e6fce9437d30ccc723ca88ed8b3aaf65616d5787
sagarpal04 <125257733+sagarpal04@users.noreply.github.com>
2025-12-09 02:45:16 +0530

    Time: 3 ms (18.79%) | Memory: 8.7 MB (69.48%) - LeetSync

--- 91-decode-ways/decode-ways.cpp
+++ 91-decode-ways/decode-ways.cpp
@@ -1,26 +1,19 @@
 class Solution {
 public:
-    vector<int> dp;
-    int solve(string s, int idx, int n){
-        if(idx >= n) return 1;
-        if(dp[idx] != -1) return dp[idx];
-        int oneDigit = s[idx] - '0';
-        int oneStep = 0;
-        if(oneDigit >= 1 && oneDigit <= 9){
-            oneStep = solve(s, idx + 1, n);
-        }
-        int twoStep = 0;
-        if(idx + 1 < n && s[idx] != '0'){
-            int twoDigit = ((s[idx] - '0') * 10 ) + (s[idx + 1] - '0');
-            if(twoDigit >= 1 && twoDigit <= 26){
-                twoStep = solve(s, idx + 2, n);
+    int numDecodings(string s) {
+        int n = s.size();
+        vector<int> dp(n + 1, 0);
+        dp[n] = 1;
+        for(int i = n - 1; i >= 0; i -= 1){
+            if(s[i] == '0') continue;
+            dp[i] += dp[i + 1];
+            if(i + 1 < n){
+                int twoDigit = (s[i] - '0') * 10 + (s[i + 1] - '0');
+                if(twoDigit >= 10 && twoDigit <= 26){
+                    dp[i] += dp[i + 2];
+                }
             }
         }
-        return dp[idx] = twoStep + oneStep;
-    }
-    int numDecodings(string s) {
-        if(s[0] == '0') return 0;
-        dp.assign(s.size(), -1);
-        return solve(s, 0, s.size());
+        return dp[0];
     }
 };
